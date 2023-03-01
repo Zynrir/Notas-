0a52e0636de03716c9d130669de02e6ca693da6f
jomil <jomil@DESKTOP-19MT1VD>
2023-03-01 20:59:29 +0100

    Utiliza las funciones de refactorización para cambiar la cabecera del método "cali". Le cambiarás el nombre por "califica" y, en el mismo paso, cambiarás el nombre del parámetro por "num_nota". El método anterior tendrá que quedar en desuso. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -1,36 +1,52 @@
 package notas;
 import java.util.Scanner;
+
 /**
  * La clase Notas es un programa que clasifica una calificación numérica
  * ingresada por el usuario y la convierte en una cadena que representa su
- * valoración, utilizando el método público cali.
+ * valoración, utilizando el método público califica.
  */
 public class Notas {
+
     /**
-     * El método público cali clasifica una calificación numérica en función de
+     * El método público califica clasifica una calificación numérica en función de
      * su valor y devuelve una cadena que representa su valoración.
-     * @param n la calificación numérica a clasificar
+     *
+     * @param num_nota la calificación numérica a clasificar
      * @return la valoración correspondiente a la calificación
      */
-public static String cali(int n) {
-  if (n < 0 || n > 10) {
-    return "El valor de la calificación introducida no es correcto";
-} else if (n < 5) {
-    return "Suspenso";
-} else if (n < 7) {
-    return "Aprobado";
-} else if (n < 9) {
-    return "Notable";
-} else {
-    return "Sobresaliente";
-   	}
-}
-public static void main(String[] args) {
-   Scanner reader = new Scanner(System.in);
-   System.out.print("Introduzca el valor de una calificación: ");
-   	int n = reader.nextInt();
-    String c = cali(n);
-   System.out.println(c);
-    reader.close();
+    public static String califica(int num_nota) {
+        if (num_nota < 0 || num_nota > 10) {
+            return "El valor de la calificación introducida no es correcto";
+        } else if (num_nota < 5) {
+            return "Suspenso";
+        } else if (num_nota < 7) {
+            return "Aprobado";
+        } else if (num_nota < 9) {
+            return "Notable";
+        } else {
+            return "Sobresaliente";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Introduzca el valor de una calificación: ");
+        int n = reader.nextInt();
+        String c = califica(n);
+        System.out.println(c);
+        reader.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
