d355b258edbce6119eaa424fcc0b3ed06aac7d94
jomil <jomil@DESKTOP-19MT1VD>
2023-03-01 20:46:25 +0100

    Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -1,28 +1,36 @@
 package notas;
-
 import java.util.Scanner;
-
+/**
+ * La clase Notas es un programa que clasifica una calificación numérica
+ * ingresada por el usuario y la convierte en una cadena que representa su
+ * valoración, utilizando el método público cali.
+ */
 public class Notas {
-
-public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int numero = 0;
-String c = "";
-System.out.print("Introduzca el valor de una calificación: ");
-numero = reader.nextInt();
-
-if (numero >= 0 && numero < 5)
-c = "Suspenso";
-else if (numero >= 5 && numero < 7)
-c = "Aprobado";
-else if (numero >= 7 && numero <9)
-c = "Notable";
-else if (numero >= 9 && numero <= 10)
-c = "Sobresaliente";
-else
-c = "El valor de la calificación introducida no es correcta";
-
-System.out.println(c);
-reader.close();
+    /**
+     * El método público cali clasifica una calificación numérica en función de
+     * su valor y devuelve una cadena que representa su valoración.
+     * @param n la calificación numérica a clasificar
+     * @return la valoración correspondiente a la calificación
+     */
+public static String cali(int n) {
+  if (n < 0 || n > 10) {
+    return "El valor de la calificación introducida no es correcto";
+} else if (n < 5) {
+    return "Suspenso";
+} else if (n < 7) {
+    return "Aprobado";
+} else if (n < 9) {
+    return "Notable";
+} else {
+    return "Sobresaliente";
+   	}
 }
+public static void main(String[] args) {
+   Scanner reader = new Scanner(System.in);
+   System.out.print("Introduzca el valor de una calificación: ");
+   	int n = reader.nextInt();
+    String c = cali(n);
+   System.out.println(c);
+    reader.close();
+    }
 }
