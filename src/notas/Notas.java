0fba8952e857e3d76081de5cc45b6dfe8c5996d2
jomil <jomil@DESKTOP-19MT1VD>
2023-03-01 21:17:49 +0100

    Crea una nueva clase que llamarás califica y, utilizando las funciones de refactorización, mueve el método "califica" a la nueva clase. Sube las modificaciones al repositorios de Github, siendo el comentario el enunciado de este ejercicio.

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -2,11 +2,27 @@ package notas;
 import java.util.Scanner;
 
 /**
- * La clase Notas es un programa que clasifica una calificación numérica
- * ingresada por el usuario y la convierte en una cadena que representa su
- * valoración, utilizando el método público califica.
+ * La clase Notas es un programa que pide una calificación numérica por teclado
+ * y la clasifica utilizando el método público califica de la clase Califica.
  */
 public class Notas {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Introduzca el valor de una calificación: ");
+        int n = reader.nextInt();
+        String c = Califica.califica(n);
+        System.out.println(c);
+        reader.close();
+    }
+}
+
+/**
+ * La clase Califica es una clase de utilidad que clasifica una calificación
+ * numérica en función de su valor y devuelve una cadena que representa su
+ * valoración.
+ */
+class Califica {
 
     /**
      * El método público califica clasifica una calificación numérica en función de
@@ -28,15 +44,8 @@ public class Notas {
             return "Sobresaliente";
         }
     }
+}
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Introduzca el valor de una calificación: ");
-        int n = reader.nextInt();
-        String c = califica(n);
-        System.out.println(c);
-        reader.close();
-    }
 }
 
 
